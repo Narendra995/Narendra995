d5f1e04415c3518d3ba08ce120af96f53c0db21c
Narender.Patidar <narender.patidar@bhavnacorp.com>
2023-03-21 12:19:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/first/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.first
+
+import android.os.Bundle
+import androidx.appcompat.R
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
